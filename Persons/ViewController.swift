52207b58a02f3b32fd5deaa552ce5ada69e1af1c
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2021-11-09 19:55:43 +0300

    initial commit

---
+++ Persons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Persons
+//
+//  Created by Михаил Зиновьев on 09.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
